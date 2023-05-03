23b3badfd201f21e545f466e8b5f4842451a6141
ezybg7 <ezybg7@umsystem.edu>
2023-05-03 15:15:11 -0500

    ok

--- client/app/login/page.js
+++ client/app/login/page.js
@@ -1,4 +1,4 @@
-import styles from '../styles/login.module.css'
+import './login.css'
 import Navbar from '../../components/navbar'
 
 const Login = () => {
@@ -7,6 +7,30 @@ const Login = () => {
       <Navbar/>
       <title>Pantry | Login</title>
       <h1>Login Page</h1>
+      <div class="container">
+        <div class="text">Login</div>
+        <form action="#">
+            <div class="data">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email"/>
+            </div>
+            <div class="data">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password"/>
+            </div>
+            <div class="forgot-pass">
+                <a href="#">Forgot Password?</a>
+            </div>
+            <div class="btn">
+                <button type="submit">login</button>
+            </div>
+            <div class="signup-link">
+                Not a member?
+                <a href="/register.html">Signup now</a>
+            </div>
+
+        </form>
+    </div>
     </div>
    );
 }
