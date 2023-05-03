e3cfbd2a4953fd9d6ab1e794e685d0fd9ff52da3
ezybg7 <ezybg7@umsystem.edu>
2023-05-03 16:24:11 -0500

    added login and register pages

--- client/app/login/register/page.js
+++ client/app/login/register/page.js
@@ -1,6 +1,5 @@
-import './login.css'
-import Link from 'next/link'
-import Navbar from '../../components/navbar'
+import './register.css'
+import Navbar from '../../../components/navbar'
 
 const Login = () => {
   return ( 
@@ -9,8 +8,12 @@ const Login = () => {
       <title>Pantry | Login</title>
       <h1>Login Page</h1>
       <div class="container">
-        <div class="text">Login</div>
+        <div class="text">Register</div>
         <form action="#">
+            <div class="data">
+                <label for="name">Username</label>
+                <input type="text" name="name" id="name"/>
+            </div>
             <div class="data">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email"/>
@@ -19,17 +22,17 @@ const Login = () => {
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password"/>
             </div>
-            <div class="forgot-pass">
-                <a href="#">Forgot Password?</a>
+            <div class="data">
+                <label for="confirm_password">Confirm Password</label>
+                <input type="confirm_password" name="confirm_password" id="confirm_password"/>
             </div>
             <div class="btn">
-                <button type="submit">login</button>
+                <button type="submit">Register</button>
             </div>
             <div class="signup-link">
-                Not a member?
-                <Link href = "/login/register"> Signup </Link>
+                Already a member?
+                <a href="/login">Login now</a>
             </div>
-
         </form>
     </div>
     </div>
