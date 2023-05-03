26d0eced33ebce4ec64c56a38b1c2b7d46ee8561
yfu03 <yannisfu@gmail.com>
2023-05-02 23:31:14 -0500

    test

---
+++ client/app/recipes/page.js
@@ -0,0 +1,13 @@
+import Navbar from '../../components/navbar'
+
+const Recipes = () => {
+  return ( 
+    <div>
+      <Navbar/>
+      <title>Pantry | Recipes</title>
+      <h1>Recipe Page</h1>
+    </div>
+   );
+}
+ 
+export default Recipes;
