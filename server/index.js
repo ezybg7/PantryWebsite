615960499656bb163aa656e89395e68374b999da
yfu03 <yannisfu@gmail.com>
2023-05-04 23:25:00 -0500

    remove require axios

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const cors = require("cors");
 const express = require("express");
 const mysql = require("mysql2");
 const app = express();
-const axios = require("axios");
 
 app.use(cors());
 app.use(express.json());
