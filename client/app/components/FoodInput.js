a9af0c5cea412db9da33c3484ebf7cc3022d5af3
yfu03 <yannisfu@gmail.com>
2023-05-04 21:57:29 -0500

    input min and max length

--- client/app/components/FoodInput.js
+++ client/app/components/FoodInput.js
@@ -1,6 +1,8 @@
 const FoodInput = ( { input, onChange }) => {
   return ( 
     <input 
+      minLength={1}
+      maxLength={30}
       input = {input} 
       onChange = { (e) => onChange(e.target.value)} 
     />
